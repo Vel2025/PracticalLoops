5ab4980924e34167b490156d042c538b2408d074
Vel2025 <velbalaiya@gmail.com>
2025-03-17 14:34:25 -0600

    First part of assignment in a summary  working

--- script.js
+++ script.js
@@ -1,43 +1,60 @@
-// Part 1: Fizz Buzz
-// To begin, solve the following classic “Fizz Buzz” problem. There are a few different ways to do this - experiment with what you think is the most efficient. Once you have solved the problem, ask yourself if there could be another way; and if so, would it be better.
+// // Part 1: Fizz Buzz
+// // To begin, solve the following classic “Fizz Buzz” problem. There are a few different ways to do this - experiment with what you think is the most efficient. Once you have solved the problem, ask yourself if there could be another way; and if so, would it be better.
 
-// Loop through all numbers from 1 to 100.
+// // Loop through all numbers from 1 to 100.
 
 
-for (let i = 1; i<101; i++) {
-    console.log(i);
-    }
+// for (let i = 1; i<101; i++) {
+//     console.log(i);
+//     }
 
-// If a number is divisible by 3, log “Fizz.”
+// // If a number is divisible by 3, log “Fizz.”
+
+// for (i=1; i<101; i++){
+//     if (i%3==0){
+//         let str1 ="Fizz"
+//         console.log(str1)
+//     }
+// }
+
+// // If a number is divisible by 5, log “Buzz.”
+
+// for (i=1; i<101; i++){
+//     if (i%5==0){
+//         let str1 ="Buzz"
+//         console.log(str1)
+//     }
+// }
+// // If a number is divisible by both 3 and 5, log “Fizz Buzz.”
+
+// for (i=1; i<101; i++){
+//     if (i%3==0&&i%5==0){
+//         let str1 ="Fizz Buzz"
+//         console.log(str1)
+//     }
+// }
+
+// // If a number is not divisible by either 3 or 5, log the number.
+// for (i=1; i<101; i++){
+//     if (i%3!==0&&i%5!==0){
+        
+//         console.log(i)
+//     }
+// }
+fit
+// combining all the above loop into one loop
 
 for (i=1; i<101; i++){
-    if (i%3==0){
-        let str1 ="Fizz"
-        console.log(str1)
-    }
-}
-
-// If a number is divisible by 5, log “Buzz.”
-
-for (i=1; i<101; i++){
-    if (i%5==0){
-        let str1 ="Buzz"
-        console.log(str1)
-    }
-}
-// If a number is divisible by both 3 and 5, log “Fizz Buzz.”
-
-for (i=1; i<101; i++){
-    if (i%3==0&&i%5==0){
+    if (i%3==0 && i%5==0){
         let str1 ="Fizz Buzz"
         console.log(str1)
-    }
-}
-
-// If a number is not divisible by either 3 or 5, log the number.
-for (i=1; i<101; i++){
-    if (i%3!==0&&i%5!==0){
-        
+    } else if (i%5==0){
+        let str1 ="Buzz"
+        console.log(str1)
+    } else if (i%3==0){
+        let str1 ="Fizz"
+        console.log(str1)
+    } else {
         console.log(i)
     }
 }
